c3aca572d735000698761c7b83eb973798e39c56
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-09-05 15:25:32 +0800

    commit

--- utils.py
+++ utils.py
@@ -1,12 +1,6 @@
-import torch as th
-import torch.nn.functional as F
+import numpy as np
 
-rmse_loss = lambda x, y: th.sqrt(F.mse_loss(x, y))
-
-def detach(x):
-    if isinstance(x, th.Tensor):
-        return x.detach()
-    elif isinstance(x, (list, tuple)):
-        return [t.detach() for t in x]
-    else:
-        return x
+def arange(a, b):
+    d = b - a
+    cs = np.cumsum(d)
+    return np.repeat(a, d) + np.arange(cs[-1]) - np.repeat(cs - d, d)
