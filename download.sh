78a7c0a3d50641b2816d7731eb69d46cb975239a
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-09-05 15:14:09 +0800

    commit

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
-wget http://files.grouplens.org/datasets/movielens/ml-100k.zip &
-wget http://files.grouplens.org/datasets/movielens/ml-1m.zip &
-wget http://files.grouplens.org/datasets/movielens/ml-10m.zip &
-wait
+wget http://files.grouplens.org/datasets/movielens/ml-100k.zip
+wget http://files.grouplens.org/datasets/movielens/ml-1m.zip
+wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
 
 for f in *.zip; do
     unzip $f &
